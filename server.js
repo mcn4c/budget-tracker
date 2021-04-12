ecca0b4cd5d898094c9447c762081400a9d76486
Mary C <mcn4c@virginia.edu>
2021-04-12 19:20:12 -0400

    deploy

--- server.js
+++ server.js
@@ -15,11 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/Budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Budget", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+       useFindAndModify: false
 });
 
 // routes
